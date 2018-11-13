8a4c0cfa453c466d70226ac8b0987c1b503f6609
hongbin-W <wanghongbin_love99@163.com>
2018-11-13 16:50:15 +0800

    自定义控件

--- library/src/main/java/com/util/library/widget/customview.java
+++ library/src/main/java/com/util/library/widget/customview.java
@@ -1,9 +1,15 @@
 package com.util.library.widget;
 
+import android.content.Context;
+
 /**
  * @description:
  * @author: whb
  * @date: 2018/11/13 15:56
  */
 public class customview {
+
+    public customview(Context context) {
+
+    }
 }
